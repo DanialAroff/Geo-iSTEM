ed47ab09d9394d48846c41c7773615f84b2fb551
Danial.A <32658241+DanialAroff@users.noreply.github.com>
2020-12-29 17:06:16 +0800

    Added script for downloading canvas

--- js/captureCanvas.js
+++ js/captureCanvas.js
@@ -1,16 +1,24 @@
-function takeShot() {
-    var modal = document.getElementById('output-modal');
-    var closeButton = document.getElementById('modalclose-button');
+var closeButton = document.getElementById('modalclose-button');
+var downloadButton = document.getElementById('download-button');
+let capturedCanvas;
+let modal = document.getElementById('output-modal');
 
+function takeShot() {
+    // clear selection first
+    tr.nodes([]);
+    selectedShapes = [];
+    layer.draw();
+
+    // generate canvas
     html2canvas(document.querySelector("#canvas")).then(canvas => {
         let canvasToBeRemoved = modal.getElementsByTagName("canvas");
         if (canvasToBeRemoved.length > 0) {
             // modal.removeChild(modal.childNodes[0]);
             canvasToBeRemoved[0].remove();
         }
+        capturedCanvas = canvas;
+
         modal.appendChild(canvas);
-        console.log(modal.innerHTML);
-        
     });
     
     modal.style.display = 'block';
@@ -20,7 +28,23 @@ function takeShot() {
             modal.style.display = 'none';
         }
     }
-    closeButton.addEventListener('click', function() {
-        modal.style.display = 'none';
-    })
+    
+    
+    return;
 }
+
+closeButton.addEventListener('click', function() {
+    modal.style.display = 'none';
+});
+
+downloadButton.addEventListener('click', function() {
+    let image = capturedCanvas.toDataURL('image/png');
+
+    let link = document.createElement('a');
+    link.href = image;
+    link.download = 'geo.png';
+    document.body.append(link);
+    link.click();
+    image = "";
+    document.body.removeChild(link);
+});
